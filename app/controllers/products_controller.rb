f2bb4780721e5be2dfb74af37dc614f15d72f310
Valeskyta <valeska.arquitecto@gmail.com>
2015-07-10 21:35:49 -0400

    transbankx

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: [:confirmation]
+  skip_before_filter :verify_authenticity_token, only: [:confirmation, :failure, :success]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
 
